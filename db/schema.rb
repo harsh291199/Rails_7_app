379da8ca8ce2390411e7aad181d219e11e51ccec
harsh291199 <harshsuthar2911@gmail.com>
2022-01-20 18:44:51 +0530

    Added model,controller and views for post

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_124607) do
+ActiveRecord::Schema.define(version: 2022_01_20_130349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.bigint "shark_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["shark_id"], name: "index_posts_on_shark_id"
+  end
 
   create_table "sharks", force: :cascade do |t|
     t.string "name"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_01_20_124607) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "sharks"
 end
